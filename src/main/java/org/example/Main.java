46c4a0cce719cb3316734105ed301a5a4e0e60b7
heyu <207867748@qq.com>
2023-07-25 15:49:13 +0800

    88

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * Description: ${NAME}
+ * Author: heyv
+ * Time: 2023 2023/7/25 15:29
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
